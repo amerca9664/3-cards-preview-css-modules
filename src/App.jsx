1089288442869958f0d9a2d90b11ab5e53163dfd
amerca9664 <amerca9664@gmail.com>
2025-06-06 14:33:31 -0500

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import Card from './components/card/Card';
+
 const App = () => {
 	return (
-		<div>
+		<>
 			<h1>Curso de React</h1>
-		</div>
+			<div className='contCard'>
+				<Card car='card--sedans' image='icon-sedans.svg' typeCar='SEDANS'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+					sapiente quo saepe illum est commodi rem iure optio, architecto
+					doloribus at dolore incidunt? Ex a asperiores dignissimos deserunt
+					quam atque!
+				</Card>
+				<Card car='card--suvs' image='icon-suvs.svg' typeCar='SUVS'>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima culpa
+					rem odit totam, soluta quis quos vitae expedita delectus temporibus
+					aspernatur ea sint modi deleniti. Rem illum laborum explicabo
+					voluptatibus.
+				</Card>
+				<Card car='card--luxury' image='icon-luxury.svg' typeCar='LUXURY'>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima culpa
+					rem odit totam, soluta quis quos vitae expedita delectus temporibus
+					aspernatur ea sint modi deleniti. Rem illum laborum explicabo
+					voluptatibus.
+				</Card>
+			</div>
+		</>
 	);
 };
 
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,18 @@
+import styles from './cardStyle.module.css';
+
+const Card = ({ car, image, typeCar, children }) => {
+	return (
+		<div className={`${styles.card}  ${styles[car]}`}>
+			<img
+				className={styles.image}
+				src={`public/assets/images/${image}`}
+				alt='sedans icon'
+			/>
+			<h2 className={styles.title}>{typeCar}</h2>
+			<p className={styles.text}>{children}</p>
+			<input className={styles.button} type='button' value='Learn more' />
+		</div>
+	);
+};
+
+export default Card;
