95d933f2193d3ba0ec47e8aba3211ace7c05a314
amerca9664 <amerca9664@gmail.com>
2025-06-12 14:39:44 -0500

    added key prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
+import { v4 } from 'uuid';
 import Card from './components/card/Card';
+const cards = [
+	{
+		classNa: 'card--sedans',
+		titleObj: 'SEDANS',
+		imageObj: 'icon-sedans.svg',
+		textObj:
+			'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, minus, perferendis deserunt voluptate praesentium fugiat et veniam nam error officiis est sunt optio eveniet rerum. Perspiciatis dolores officiis dolor doloremque!'
+	},
+	{
+		classNa: 'card--suvs',
+		titleObj: 'SUVS',
+		imageObj: 'icon-suvs.svg',
+		textObj:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi odio natus molestiae ipsum quisquam perspiciatis nam tempora, quos voluptates, commodi pariatur reprehenderit? Suscipit eaque perferendis dolorum omnis molestias, magni autem?'
+	},
+	{
+		classNa: 'card--luxury',
+		titleObj: 'LUXURY',
+		imageObj: 'icon-luxury.svg',
+		textObj:
+			' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, voluptates adipisci voluptas, perferendis, odio vitae aliquid blanditiis delectus tenetur quas animi beatae neque nobis deserunt quo suscipit sed distinctio accusantium!'
+	}
+];
+
+const cardInfo = iteam => (
+	<Card
+		key={v4()}
+		car={iteam.classNa}
+		typeCar={iteam.titleObj}
+		image={iteam.imageObj}
+	>
+		{iteam.textObj}
+	</Card>
+);
 
 const App = () => {
 	return (
 		<>
 			<h1>Curso de React</h1>
-			<div className='contCard'>
-				<Card car='card--sedans' image='icon-sedans.svg' typeCar='SEDANS'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-					sapiente quo saepe illum est commodi rem iure optio, architecto
-					doloribus at dolore incidunt? Ex a asperiores dignissimos deserunt
-					quam atque!
-				</Card>
-				<Card car='card--suvs' image='icon-suvs.svg' typeCar='SUVS'>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima culpa
-					rem odit totam, soluta quis quos vitae expedita delectus temporibus
-					aspernatur ea sint modi deleniti. Rem illum laborum explicabo
-					voluptatibus.
-				</Card>
-				<Card car='card--luxury' image='icon-luxury.svg' typeCar='LUXURY'>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima culpa
-					rem odit totam, soluta quis quos vitae expedita delectus temporibus
-					aspernatur ea sint modi deleniti. Rem illum laborum explicabo
-					voluptatibus.
-				</Card>
-			</div>
+			<div className='contCard'>{cards.map(cardInfo)}</div>
 		</>
 	);
 };
